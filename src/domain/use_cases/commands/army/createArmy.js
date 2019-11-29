2d3d3f53b02e38496b1879bbcd130d01c434a8b5
Milan Timotijevic <milantimotijevic@Milans-MacBook-Pro.local>
2019-11-29 21:48:59 +0100

    rename prop

--- src/domain/use_cases/commands/army/createArmy.js
+++ src/domain/use_cases/commands/army/createArmy.js
@@ -2,7 +2,7 @@ const ArmyRepository = require('../../../../repository/ArmyRepository');
 
 module.exports = function createArmy(armyParam) {
 	// TODO look into ESlint settings to determine why spread operator is disallowed
-	armyParam.current = armyParam.units;
+	armyParam.currentUnits = armyParam.units;
 	// return ArmyRepository.createArmy({...armyParam, currentUnits: armyParam.units});
 	return ArmyRepository.createArmy(armyParam);
 };
--- src/repository/model/Army.js
+++ src/repository/model/Army.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const schema = new mongoose.Schema({
 	name: 'String',
 	units: 'Number',
-	startingUnits: 'Number',
+	currentUnits: 'Number',
 	defeated: { type: 'Boolean', defaultValue: false },
 	reload: 'Number',
 });
