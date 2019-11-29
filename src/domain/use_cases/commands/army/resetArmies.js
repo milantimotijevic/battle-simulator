9c5abba4991545692081fd4bf4d6c1c1f96bd2ca
Milan Timotijevic <milantimotijevic@Milans-MacBook-Pro.local>
2019-11-29 16:57:09 +0100

    battle resetting cleanup

--- src/domain/use_cases/commands/army/resetArmies.js
+++ src/domain/use_cases/commands/army/resetArmies.js
@@ -1,5 +1,6 @@
 const ArmyRepository = require('../../../../repository/ArmyRepository');
 
 module.exports = function resetArmies(armyIds) {
+	// TODO kill workers here
 	return ArmyRepository.resetArmies(armyIds);
 };
--- src/domain/use_cases/commands/battle/resetBattle.js
+++ src/domain/use_cases/commands/battle/resetBattle.js
@@ -3,10 +3,10 @@ const resetArmies = require('../../commands/army/resetArmies');
 const updateBattle = require('./updateBattle');
 
 module.exports = async function resetBattle(battleId) {
-	const battle = await BattleRepository.findOne(battleId);
+	let battle = await BattleRepository.findOne(battleId);
 	// TODO extract armies and reset their unit count
 	// TODO check battle status
 	// TODO ensure these are IDs
 	await resetArmies(battle.armies);
-	return updateBattle(battle.id, { logs: [] });
+	battle = await updateBattle(battle.id, { logs: [] });
 };
