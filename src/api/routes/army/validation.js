b8f72359f569e6f5b1e97d4fa4fb0c7ecd116212
Milan Timotijevic <milantimotijevic@Milans-MacBook-Pro.local>
2019-11-29 12:37:17 +0100

    add validation for army endpoints

--- src/api/routes/army/validation.js
+++ src/api/routes/army/validation.js
@@ -1,10 +1,21 @@
 const Joi = require('@hapi/joi');
 
-const armyIdParam = {};
+const armyIdParam = {
+	id: Joi.string().length(24).required(),
+};
 
-const newArmySchema = {};
+const newArmySchema = {
+	name: Joi.string().required(),
+	// TODO add error message for int out of bounds
+	units: Joi.number().integer().biggerEquals(80).lowerEquals(100)
+		.required(),
+	strategy: Joi.string().valid('RANDOM', 'WEAKEST', 'STRONGEST').required(),
+};
 
-const addArmyToBattleParams = {};
+const addArmyToBattleParams = {
+	battleId: Joi.string().length(24).required(),
+	armyId: Joi.string().length(24).required(),
+};
 
 module.exports = {
 	armyIdParam,
