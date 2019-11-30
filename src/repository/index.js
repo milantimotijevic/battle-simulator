94c11b1af89c854fdc96fae1d654fcf1e9c44d6e
Milan Timotijevic <milantimotijevic@Milans-MacBook-Pro.local>
2019-11-30 19:28:35 +0100

    better err handling for db connection

--- src/repository/index.js
+++ src/repository/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+console.log('Initiating connection with MongoDB');
 /**
  * Initiates connection with MongoDB and immediately loads models
  * Other modules will have easy access to the DB via requiring 'mongoose'
@@ -13,8 +14,13 @@ mongoose.connect(process.env.DATABASE_CONNECTION_STRING, {
 });
 
 const db = mongoose.connection;
-// TODO handle connection error better
-db.on('error', console.error.bind(console, 'connection error:'));
+/**
+ * This unhandled error will cause the application to crash in case connection with MongoDB cannot be established
+ * We are currently OK with that
+ */
+db.on('error', () => {
+	throw new Error('Error connecting to MongoDB');
+});
 db.once('open', () => {
-	console.log('Connection with DB established');
+	console.log('Connection with MongoDB established');
 });
