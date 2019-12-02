be66f2848f37155bf7eb7a1ad7085b027a2e81b9
Milan Timotijevic <milantimotijevic@Milans-MacBook-Pro.local>
2019-12-02 15:22:19 +0100

    await announcements

--- src/domain/use_cases/commands/army/registerDamage.js
+++ src/domain/use_cases/commands/army/registerDamage.js
@@ -3,9 +3,6 @@ const announce = require('../battle/announce');
 
 /**
  * Persists changes to an army's currentUnits count and marks it as defeated if needed
- *
- * We do not want to await this method, nor do we want to await anything called inside it
- * There's no time for that, we have a battle to win, for heck's sake!
  */
 module.exports = async function registerDamage(battle, army, value) {
 	const currentUnits = army.currentUnits - value;
--- src/domain/use_cases/commands/battle/announce.js
+++ src/domain/use_cases/commands/battle/announce.js
@@ -3,10 +3,9 @@ const { format } = require('../../../use_cases/workers/helpers');
 
 /**
  * Logs a battle-related action and persists it in battle's log
- * Don't want to wait for anything
  */
-module.exports = function announce(battle, messageBody) {
+module.exports = async function announce(battle, messageBody) {
 	const message = `BATTLE INFO - ${format(battle)}): ${messageBody}`;
 	console.log(message);
-	BattleRepository.pushToArrayProperty(battle.id, 'log', message);
+	await BattleRepository.pushToArrayProperty(battle.id, 'log', message);
 };
