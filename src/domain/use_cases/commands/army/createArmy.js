eb79f54b282a40cd71953a27273ca676880ddfe0
Milan Timotijevic <milantimotijevic@Milans-MacBook-Pro.local>
2019-12-04 00:05:00 +0100

    ensure army gets defeated flag (false) when created; update postman

--- src/domain/use_cases/commands/army/createArmy.js
+++ src/domain/use_cases/commands/army/createArmy.js
@@ -3,6 +3,7 @@ const ArmyRepository = require('../../../../repository/ArmyRepository');
 module.exports = function createArmy(armyParam) {
 	// TODO look into ESlint settings to determine why spread operator is disallowed
 	armyParam.currentUnits = armyParam.units;
+	armyParam.defeated = false;
 	// return ArmyRepository.createArmy({...armyParam, currentUnits: armyParam.units});
 	return ArmyRepository.createArmy(armyParam);
 };
